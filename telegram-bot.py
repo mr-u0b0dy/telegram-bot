bd7ed3c88e9b8a3adb9bfac28001b22aca3e4f79
Dharun-palanisamy <dharunpalanisamy.plrd@gmail.com>
2021-05-19 16:49:34 +0530

    added short url functionality

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,25 +1,58 @@
-# import telegram
-# from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-# bot = telegram.bot(token='') 
-# updater = Updater(token='', use_context=True) 
-# dispatcher = updater.dispatcher
-# def hello(update, context):
-#     context.bot.send_message(chat_id=update.effective_chat.id, text='Hello, World')
-# hello_handler = CommandHandler('hello', hello)
-# dispatcher.add_handler(hello_handler)
-# updater.start_polling()
-
+import telegram
+import requests
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from time import sleep
+from telegram.ext import Updater, CommandHandler, CallbackContext, InlineQueryHandler, MessageHandler, Filters, ConversationHandler
+from telegram.chataction import ChatAction
+from telegram.bot import Bot
+from telegram.update import Update
 
+updater = Updater('1843979701:AAFFJ2T1Cy-Y6D2Ab5DPoAPlB_su8uJrVCQ', use_context=True)
+
+def start(update, context):
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    update.message.reply_text('Hi\nThis Bot is developed by APD🌷\nTo Know the commands in this Bot🤖 enter /help')
+
+def help(update, context):
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    update.message.reply_text('This Bot uses following commands\n/hello\n/help\n/shorturl - Shortens a long URL to short Url')
 
 def hello(update: Update, context: CallbackContext) -> None:
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
     update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
+def short_url (update: Update, context: CallbackContext):
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    url_json={'url':update['message']['text']}
+    short_url=requests.post('https://cleanuri.com/api/v1/shorten',url_json)
+    if short_url.status_code==200:
+        update.message.reply_text(short_url.json()['result_url'])
+    else:
+        update.message.reply_text(short_url.json()['error']+"\nerror code:"+str(short_url.status_code))
+    return ConversationHandler.END
 
-updater = Updater('1843979701:AAFFJ2T1Cy-Y6D2Ab5DPoAPlB_su8uJrVCQ')
+def short_url_callback(update: Update, context: CallbackContext) :
+    context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+    update.message.reply_text('Enter the long URL to be shorten\nEnter /cancel to cancel')
+    telegram.ForceReply(force_reply=True)
+    return 'SHURL' 
 
+def cancel (update: Update, context: CallbackContext) -> int:
+    update.message.reply_text('Process is canceled succesfuly👍\nTry other commands as well')
+    return ConversationHandler.END
+
+short_url_conv= ConversationHandler(
+    entry_points=[CommandHandler('shorturl', short_url_callback)],
+    states={
+        'SHURL':[MessageHandler(Filters.text& ~Filters.command,short_url) ]
+    },
+    fallbacks=[CommandHandler('cancel',cancel)]
+)
+
+updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+updater.dispatcher.add_handler(short_url_conv)
 
 updater.start_polling()
 updater.idle()
